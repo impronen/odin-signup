3d6d94029db469c278086608a5663420b265b83d
impronen <impronen@gmail.com>
2022-10-26 20:40:57 +0300

    ADD script.js + js validation framework

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let pwdInput = document.getElementById("user_password");
+let pwdInputRepeat = document.getElementById("user_password_repeat");
+
+pwdInputRepeat.onblur = function () {
+if (pwdInput === pwdInputRepeat) {
+    // Add the class of :valid
+}
+else {
+    // Add the class of :invalid
+}
+}
+
+
+
+/* classList.add('selectedButton')  */
