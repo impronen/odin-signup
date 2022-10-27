ea8e3b8c6d9a487bc28857cb9dca4ece49da5ff3
impronen <impronen@gmail.com>
2022-10-27 10:02:00 +0300

    PWD validation Password validation made in JS.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 
 let pwdInput = document.getElementById("user_password");
 let pwdInputRepeat = document.getElementById("user_password_repeat");
+let pwdMessage = document.getElementById('pwd-message');
+
+
 
 pwdInputRepeat.onblur = function () {
 if (pwdInput === pwdInputRepeat) {
-    // Add the class of :valid
+    pwdInput.classList.add('validPassword');
+    pwdInputRepeat.classList.add('validPassword');
+} else {
+    pwdInput.classList.add('invalidPassword');
+    pwdInputRepeat.classList.add('invalidPassword');
+    pwdMessage = "The passwords don't match";
 }
-else {
-    // Add the class of :invalid
 }
-}
-
-
-
-/* classList.add('selectedButton')  */
